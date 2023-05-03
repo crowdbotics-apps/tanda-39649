31dd6168abe66a1609fbc5ade3ef937231cc57d1
Slobodan Petrovic <slobodan.petrovic@crowdbotics.com>
2023-05-03 20:13:33 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI10/index.js
+++ screens/ScreenAI10/index.js
@@ -1,20 +1,18 @@
-import { useNavigation } from "@react-navigation/native";
-import { Pressable } from "react-native";
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
-const TandaScreen = () => {
-  const navigation = useNavigation();
+const ProfileScreen = () => {
   return <View style={styles.container}>
-      <Image source={{
-      uri: 'https://dummyimage.com/300x300/000/fff'
-    }} style={styles.image} />
-      <Pressable onPress={() => {
-      navigation.navigate("ScreenAI9");
-    }}><Text style={styles.title}>Tanda Title</Text></Pressable>
-      <Pressable><Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, sapien vel bibendum bibendum, velit sapien bibendum sapien, vel bibendum sapien velit.</Text></Pressable>
-      <View style={styles.infoContainer}>
-        <Text style={styles.info}>Period: 1 month</Text>
-        <Text style={styles.info}>Participants: 5</Text>
+      <TouchableOpacity style={styles.backButton}>
+        <Text style={styles.backButtonText}>Back</Text>
+      </TouchableOpacity>
+      <View style={styles.profileContainer}>
+        <Image source={{
+        uri: 'https://dummyimage.com/200x200/000/fff'
+      }} style={styles.profileImage} />
+        <Text style={styles.profileName}>John Doe</Text>
+        <Text style={styles.profileAddress}>123 Main St, Anytown USA</Text>
+        <Text style={styles.tandaCount}>Total Tandas Participated: 5</Text>
       </View>
     </View>;
 };
@@ -22,31 +20,43 @@ const TandaScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingTop: 50
   },
-  image: {
+  backButton: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#ccc',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 20
+  },
+  backButtonText: {
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  profileContainer: {
+    alignItems: 'center'
+  },
+  profileImage: {
     width: 200,
     height: 200,
     borderRadius: 100,
     marginBottom: 20
   },
-  title: {
+  profileName: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10
   },
-  description: {
-    textAlign: 'center',
-    marginHorizontal: 20,
+  profileAddress: {
+    fontSize: 16,
     marginBottom: 20
   },
-  infoContainer: {
-    flexDirection: 'row'
-  },
-  info: {
-    marginRight: 20
+  tandaCount: {
+    fontSize: 18,
+    fontWeight: 'bold'
   }
 });
-export default TandaScreen;
+export default ProfileScreen;
